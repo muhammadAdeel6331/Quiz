add443396a35b1ac0a06373fcaf6a528796cb6b6
muhammadAdeel6331 <57092891+muhammadAdeel6331@users.noreply.github.com>
2020-08-10 07:35:35 -0700

    Assignment_11_Quiz
    Assignment_11_Quiz

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ function checking(){
     var q1_a3= document.getElementById("1q-3a");
     var q1_a4= document.getElementById("1q-1a"); 
     if (right_a_1.checked == true){
-        result++
-        alert('Question 1 is correct')
+        result+=10
+        //alert('Question 1 is correct')
     }
     else{
-        alert('Question 1 is not correct');    
+        //alert('Question 1 is not correct');    
     }
 
 
@@ -18,11 +18,11 @@ function checking(){
     var q2_a3= document.getElementById("2q-3a");
     var q2_a4= document.getElementById("2q-4a"); 
     if (right_a_2.checked == true){
-        result++
-        alert('Question 2 is correct')
+        result+=10
+        //alert('Question 2 is correct')
     }
     else{
-        alert('Question 2 is not correct');    
+        //alert('Question 2 is not correct');    
     }
 
 
@@ -32,11 +32,11 @@ function checking(){
     var q3_a3= document.getElementById("3q-3a");
     var q3_a4= document.getElementById("3q-4a"); 
     if (right_a_3.checked == true){
-        result++
-        alert('Question 3 is correct')
+        result+=10
+        //alert('Question 3 is correct')
     }
     else{
-        alert( 'Question 3 is not correct');    
+        //alert( 'Question 3 is not correct');    
     }
 
 
@@ -46,11 +46,11 @@ function checking(){
     var q4_a3= document.getElementById("4q-3a");
     var q4_a4= document.getElementById("4q-4a"); 
     if (right_a_4.checked == true){
-        result++
-        alert('Question 4 is correct')
+        result+=10
+        //alert('Question 4 is correct')
     }
     else{
-        alert( 'Question 4 is not correct');    
+        //alert( 'Question 4 is not correct');    
     }
 
 
@@ -60,8 +60,8 @@ function checking(){
     var q5_a3= document.getElementById("5q-3a");
     var q5_a4= document.getElementById("5q-4a"); 
     if (right_a_5.checked == true){
-        result++
-        alert('Question 5 is correct')
+        result+=10
+        //alert('Question 5 is correct')
     }
     else{
         alert( 'Question 5 is not correct');    
@@ -69,23 +69,20 @@ function checking(){
     alert("Your result is : "+ result)
 
 
-
-
-
-    var divmain= document.getElementById("main")
+    // var divmain= document.getElementById("main")
     var divvv = document.getElementById("result")
-
-    
-
     divvv.setAttribute("class","result")
-    divmain.setAttribute("class","main")
+    // divmain.setAttribute("class","main")
 
 
-    var resultt= result
+    var resultt= "Your result is : "+result
     var para = document.createElement('p')
-    var paratext = document.createTextNode(resultt.value)
-    console.log(paratext)
-    alert(para.appendchild(paratext))
+    // var paratext = document.createTextNode(resultt.value)
+    // para.appendChild(paratext)
+    para.innerHTML = resultt
+    console.log(para)
+    var text ="your res"
+    divvv.appendChild(para)
 
     
     
